820a5433079c2222ecb41c34e3f09155689e2103
ulisesStar <ulises@starbrand.mx>
2018-08-20 11:54:10 -0500

    ultimo

--- http/connection.js
+++ http/connection.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var mysql = require('mysql');
 var Sequelize = require('sequelize');
 /*
@@ -14,21 +15,28 @@ var sequelize = new Sequelize('heroku_8336c91bfbfd82e', 'bebb7ad19454a6', '28d75
 });
 */
 
-var sequelize = new Sequelize('ideductos', 'root', 'root', {
-    host: '35.185.98.151',
-    dialect: 'mysql',
-    port: '3306',
+var sequelize = new Sequelize('ideductos', 'jessem', '#1q2w3e4r', {
+    host: 'jessem.database.windows.net',
+    dialect: 'mssql',
+    port: '1433',
     pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
+        min: 10,
+        max: 300,
+        idle: 30000,
+    },
+    dialectOptions: {
+        requestTimeout : 30000,
+        encrypt: true,
+        ssl: {
+            ca: fs.readFileSync('ssl.crt.pem')
+        }
     }
 });
 
-
-sequelize.sync()
-.then(() =>  console.log('Connecion realizada'))
-.catch(err =>  console.log('No se puede conectar a la bd:', err))
+// 
+// sequelize.sync()
+// .then(() =>  console.log('Connecion realizada'))
+// .catch(err =>  console.log('No se puede conectar a la bd:', err))
 
 
 module.exports.Sequelize = Sequelize;
