dfa8dee9eb97161994b28c6a99d51b5a8c2e6a77
Ulises Gonzales <ulisesgonzales@MacBook-Pro-de-Ulises.local>
2018-09-30 12:36:02 -0500

    vale

--- http/connection.js
+++ http/connection.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var mysql = require('mysql');
 var Sequelize = require('sequelize');
-/*
 
-var sequelize = new Sequelize('heroku_8336c91bfbfd82e', 'bebb7ad19454a6', '28d7532c', {
-    host: 'us-cdbr-iron-east-04.cleardb.net',
+
+var sequelize = new Sequelize('ideductos', 'root', '1234', {
+    host: '35.184.49.255',
     dialect: 'mysql',
     port: '3306',
     pool: {
@@ -13,30 +13,30 @@ var sequelize = new Sequelize('heroku_8336c91bfbfd82e', 'bebb7ad19454a6', '28d75
         idle: 10000
     }
 });
-*/
 
-var sequelize = new Sequelize('ideductos', 'jessem', '#1q2w3e4r', {
-    host: 'jessem.database.windows.net',
-    dialect: 'mssql',
-    port: '1433',
-    pool: {
-        min: 10,
-        max: 300,
-        idle: 30000,
-    },
-    dialectOptions: {
-        requestTimeout : 30000,
-        encrypt: true,
-        ssl: {
-            ca: fs.readFileSync('ssl.crt.pem')
-        }
-    }
-});
 
-// 
-// sequelize.sync()
-// .then(() =>  console.log('Connecion realizada'))
-// .catch(err =>  console.log('No se puede conectar a la bd:', err))
+// var sequelize = new Sequelize('ideductos', 'jessem', '#1q2w3e4r', {
+//     host: 'jessem.database.windows.net',
+//     dialect: 'mssql',
+//     port: '1433',
+//     pool: {
+//         min: 10,
+//         max: 300,
+//         idle: 30000,
+//     },
+//     dialectOptions: {
+//         requestTimeout : 30000,
+//         encrypt: true,
+//         ssl: {
+//             ca: fs.readFileSync('ssl.crt.pem')
+//         }
+//     }
+// });
+
+
+sequelize.sync()
+.then(() =>  console.log('Connecion realizada'))
+.catch(err =>  console.log('No se puede conectar a la bd:', err))
 
 
 module.exports.Sequelize = Sequelize;
